7c87f7bca74a291093f8d8dcb05312a602df22a5
Kathi <51385843+Kirariaaa@users.noreply.github.com>
2019-06-07 12:36:02 +0200

    Update Tisch.java

--- Restaurant/src/restaurant/Tisch.java
+++ Restaurant/src/restaurant/Tisch.java
@@ -4,11 +4,61 @@
  * and open the template in the editor.
  */
 package restaurant;
+import java.util.Random;
 
 /**
  *
- * @author Thomas Weber
+ * @author Katharina Lainer
  */
-public class Tisch {
+public class Tisch{
     
+    private int stuhlzahl;
+    private int tischnr;
+    private int besetzt;
+    private Random number;
+    
+    public Tisch (int sz, int nr){
+        
+        stuhlzahl = sz;
+        tischnr = nr;
+        besetzt = 0;
+        number = new Random();
+    }
+    
+    public boolean gaesteKommen (int anzahl){
+        
+        if(stuhlzahl < anzahl){
+            
+            return false;
+        }else{
+            
+            besetzt = anzahl;
+            return true;
+        }
+    }
+    
+    public void gaesteGehen(){
+        
+        besetzt = 0;
+    }
+    
+    public int stuhlZahlGeben(){
+        
+        return stuhlzahl;
+    }
+    
+    public int gaesteZahlGeben(){
+        
+        return besetzt;
+    }
+    
+    public void datenAusgeben(){
+        
+        System.out.println("Tisch " + tischnr + ": Gästezahl = " + besetzt + " von " + stuhlzahl);
+    }
+    
+    public int tischNummerGeben(){
+        
+        return tischnr;
+    }
 }
